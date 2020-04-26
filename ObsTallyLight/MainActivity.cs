ba90ba46ac692faac4fdde5c736a2577a0ccb045
tbar0012 <tom@bartindale.com>
2020-04-26 22:45:00 +1000

    init android to fb

--- ObsTallyLight/MainActivity.cs
+++ ObsTallyLight/MainActivity.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
-using OBS.WebSocket.NET;
-using OBS.WebSocket.NET.Types;
-using Xamarin.Essentials;
+using AndroidX.AppCompat.App;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Google.Android.Material.Snackbar;
 
 namespace ObsTallyLight
 {
     [Activity(Label = "@string/app_name")]
     public class MainActivity : AppCompatActivity
     {
-        private ObsWebSocket _obs;
+
+        FirebaseClient FirebaseClient = new FirebaseClient("https://obstally.firebaseio.com/");
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -32,8 +30,6 @@ namespace ObsTallyLight
 
             FindViewById<ImageButton>(Resource.Id.settings).Click += OpenSettingsClick;
             FindViewById<Button>(Resource.Id.save).Click += SaveClick;
-
-            _obs = new ObsWebSocket();
         }
 
         private void SaveClick(object sender, EventArgs e)
@@ -42,27 +38,9 @@ namespace ObsTallyLight
             
             FindViewById(Resource.Id.settingspage).Visibility = ViewStates.Gone;
 
-
-            Xamarin.Essentials.Preferences.Set("server", FindViewById<TextView>(Resource.Id.server).Text.ToString());
-            Xamarin.Essentials.Preferences.Set("port", FindViewById<TextView>(Resource.Id.port).Text.ToString());
-
-
-
-
-            var server = Xamarin.Essentials.Preferences.Get("server", "10.0.2.2");
-            var port = Xamarin.Essentials.Preferences.Get("port", "4444");
-
-            Intent intent = Intent;
-            Finish();
-            StartActivity(intent);
-        }
-
-        protected override void OnPause()
-        {
-            base.OnPause();
-            _obs.Disconnected -= _obs_Disconnected;
-            _obs.Disconnect();
-            _obs = null;
+            //Intent intent = Intent;
+            //Finish();
+            //StartActivity(intent);
         }
 
         private void OpenSettingsClick(object sender, EventArgs e)
@@ -75,15 +53,22 @@ namespace ObsTallyLight
 
         bool firstrun = true;
 
-        protected override void OnResume()
+        class ObsInstance
+        {
+            //public string MachineId { get; set; }
+            public string Name { get; set; }
+            public bool Online { get; set; }
+            public string Pwd { get; set; }
+            public bool Recording { get; set; }
+            public bool Streaming { get; set; }
+            public List<string> Tally { get; set; }
+            public List<string> PreviewTally { get; set; }
+            public List<string> Sources { get; set; }
+        }
+
+        protected async override void OnResume()
         {
             base.OnResume();
-            _obs.Connected += _obs_Connected;
-            _obs.Disconnected += _obs_Disconnected;
-            _obs.OBSExit += _obs_OBSExit;
-            _obs.StreamingStateChanged += _obs_StreamingStateChanged;
-            _obs.RecordingStateChanged += _obs_RecordingStateChanged;
-            _obs.SceneChanged += _obs_SceneChanged;
             //_obs.SceneItemVisibilityChanged += _obs_SceneItemVisibilityChanged;
             //_obs.TransitionBegin += _obs_TransitionBegin;
 
@@ -91,40 +76,62 @@ namespace ObsTallyLight
             FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
             FindViewById(Resource.Id.settingspage).Visibility = ViewStates.Gone;
 
-            var server = Xamarin.Essentials.Preferences.Get("server", "10.0.2.2");
-            FindViewById<TextView>(Resource.Id.server).Text = server;
-
-            var port = Xamarin.Essentials.Preferences.Get("port", "4444");
-            FindViewById<TextView>(Resource.Id.port).Text = port;
-
+            CurrentInstance = Xamarin.Essentials.Preferences.Get("instance", null);
             CurrentSource = Xamarin.Essentials.Preferences.Get("source", 0);
 
             FindViewById<ListView>(Resource.Id.sources).ItemClick += MainActivity_ItemClick;
 
 
-            
+
+            var instances = await FirebaseClient.Child("instances").OnceAsync<ObsInstance>();
+
+            var instance = instances.First();
 
 
-            (new Thread(() =>
+            var labels = instance.Object.Sources;
+            labels.Insert(0, "[PROGRAM OUTPUT]");
+            RunOnUiThread(() =>
             {
-                try
+                FindViewById<ListView>(Resource.Id.sources).Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItemSingleChoice, labels);
+                    FindViewById<ListView>(Resource.Id.sources).SetSelection(CurrentSource);
+                });
+
+
+            var observeTally = FirebaseClient.Child("instances").Child(instance.Key).Child("Tally").AsObservable<List<string>>().Subscribe(tally =>
+            {
+                UpdateTally(tally.Object);
+            });
+
+            var observeStreaming = FirebaseClient.Child("instances").Child(instance.Key).Child("Streaming").AsObservable<bool>().Subscribe(streaming =>
+            {
+                UpdateStreaming(streaming.Object);
+            });
+        }
+
+        void UpdateRecording(bool yes)
+        {
+            RECORDING = yes;
+        }
+
+        void UpdateStreaming(bool yes)
+        {
+            STREAMING = yes;
+        }
+
+        void UpdateTally(List<string> tally)
+        {
+            if (RECORDING)
+            {
+                //var scene = _obs.Api.GetCurrentScene();
+                if (tally.Contains(sources[CurrentSource]))
                 {
-                    _obs.Connect($"ws://{server}:{port}", "");
+                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
                 }
-                catch { }
-            })).Start();
-
-        }
-
-        private void _obs_TransitionBegin(object sender, EventArgs e)
-        {
-            
-            
-        }
-
-        private void _obs_SceneItemVisibilityChanged(ObsWebSocket sender, string sceneName, string itemName)
-        {
-            
+                else
+                {
+                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
+                }
+            }
         }
 
         private void MainActivity_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
@@ -134,21 +141,21 @@ namespace ObsTallyLight
             CurrentSource = e.Position;
         }
 
-        private void _obs_SceneChanged(ObsWebSocket sender, string newSceneName)
-        {
-            if (RECORDING)
-            {
-                var scene = _obs.Api.GetCurrentScene();
-                if (scene.Items.Exists(s => s.SourceName == sources[CurrentSource].Name))
-                {
-                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
-                }
-                else
-                {
-                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
-                }                    
-            }
-        }
+        //private void _obs_SceneChanged(ObsWebSocket sender, string newSceneName)
+        //{
+        //    if (RECORDING)
+        //    {
+        //        var scene = _obs.Api.GetCurrentScene();
+        //        if (scene.Items.Exists(s => s.SourceName == sources[CurrentSource].Name))
+        //        {
+        //            FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
+        //        }
+        //        else
+        //        {
+        //            FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
+        //        }                    
+        //    }
+        //}
 
         private void MainActivity_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
@@ -158,137 +165,124 @@ namespace ObsTallyLight
 
         }
 
-        private void _obs_Disconnected(object sender, EventArgs e)
-        {
-            if (firstrun)
-            {
-                firstrun = false;
-                return;
-            }
-            //obs disconnected
-            RunOnUiThread(() =>
-            {
-                FindViewById<ImageView>(Resource.Id.connected).SetImageResource(Resource.Drawable.power_plug_off);
-                FindViewById(Resource.Id.connectingprogres).Visibility = ViewStates.Visible;
-            });
+        //private void _obs_Disconnected(object sender, EventArgs e)
+        //{
+        //    if (firstrun)
+        //    {
+        //        firstrun = false;
+        //        return;
+        //    }
+        //    //obs disconnected
+        //    RunOnUiThread(() =>
+        //    {
+        //        FindViewById<ImageView>(Resource.Id.connected).SetImageResource(Resource.Drawable.power_plug_off);
+        //        FindViewById(Resource.Id.connectingprogres).Visibility = ViewStates.Visible;
+        //    });
+        //}
 
-            (new Thread(() => {
-                //while (!_obs.IsConnected)
-                //{
-                    //Thread.Sleep(5000);
-                    var server = Xamarin.Essentials.Preferences.Get("server", "10.0.2.2");
-                    var port = Xamarin.Essentials.Preferences.Get("port", "4444");
-                    _obs.Connect($"ws://{server}:{port}", "");
-                //}
-            })).Start();
-        }
 
-        private void _obs_OBSExit(object sender, EventArgs e)
-        {
-            //obs not running
-        }
-
-        List<SourceInfo> sources;
+        List<string> sources;
         int CurrentSource = 0;
+        private string CurrentInstance;
         bool RECORDING = false;
         bool STREAMING = false;
         
 
-        private void _obs_Connected(object sender, EventArgs e)
-        {
-            //obs connected
-            RunOnUiThread(() =>
-            {
-                FindViewById<ImageView>(Resource.Id.connected).SetImageResource(Resource.Drawable.power_plug);
-                FindViewById(Resource.Id.connectingprogres).Visibility = ViewStates.Gone;
-            });
+        //private void _obs_Connected(object sender, EventArgs e)
+        //{
+        //    //obs connected
+        //    RunOnUiThread(() =>
+        //    {
+        //        FindViewById<ImageView>(Resource.Id.connected).SetImageResource(Resource.Drawable.power_plug);
+        //        FindViewById(Resource.Id.connectingprogres).Visibility = ViewStates.Gone;
+        //    });
 
-            var streaming = _obs.Api.GetStreamingStatus();
-            RunOnUiThread(() =>
-            {
-                if (CurrentSource == 0)
-                {
-                    if (streaming.IsRecording)
-                        FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
-                    if (streaming.IsStreaming)
-                        FindViewById(Resource.Id.live).Visibility = ViewStates.Visible;
-                }
-            });
+        //    var streaming = _obs.Api.GetStreamingStatus();
+        //    RunOnUiThread(() =>
+        //    {
+        //        if (CurrentSource == 0)
+        //        {
+        //            if (streaming.IsRecording)
+        //                FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
+        //            if (streaming.IsStreaming)
+        //                FindViewById(Resource.Id.live).Visibility = ViewStates.Visible;
+        //        }
+        //    });
 
-            //list sources:
-            sources = _obs.Api.GetSourcesList();
+        //    //list sources:
+        //    sources = _obs.Api.GetSourcesList();
             
-            var labels = (from s in sources select s.Name).ToList();
-            labels.Insert(0,"[PROGRAM OUTPUT]");
-            RunOnUiThread(() =>
-            {
-                FindViewById<ListView>(Resource.Id.sources).Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItemSingleChoice, labels);
-                //FindViewById<ListView>(Resource.Id.sources).Post(() =>
-                //{
+        //    var labels = (from s in sources select s.Name).ToList();
+        //    labels.Insert(0,"[PROGRAM OUTPUT]");
+        //    RunOnUiThread(() =>
+        //    {
+        //        FindViewById<ListView>(Resource.Id.sources).Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItemSingleChoice, labels);
+        //        //FindViewById<ListView>(Resource.Id.sources).Post(() =>
+        //        //{
 
 
-                    FindViewById<ListView>(Resource.Id.sources).SetSelection(CurrentSource);
-                    //FindViewById<ListView>(Resource.Id.sources).SelectedView.Selected = true;
-                //});
-            });
+        //            FindViewById<ListView>(Resource.Id.sources).SetSelection(CurrentSource);
+        //            //FindViewById<ListView>(Resource.Id.sources).SelectedView.Selected = true;
+        //        //});
+        //    });
 
 
 
-        }
+        //}
 
-        private void _obs_RecordingStateChanged(ObsWebSocket sender, OBS.WebSocket.NET.Types.OutputState type)
-        {
-            if (type == OutputState.Started)
-                RECORDING = true;
-            else
-                RECORDING = false;
+        //private void _obs_RecordingStateChanged(ObsWebSocket sender, OBS.WebSocket.NET.Types.OutputState type)
+        //{
+        //    if (type == OutputState.Started)
+        //        RECORDING = true;
+        //    else
+        //        RECORDING = false;
 
-            if (CurrentSource == 0)
-            {
-                RunOnUiThread(() =>
-                {
-                    //recording started / stopped
-                    switch (type)
-                    {
-                        case OBS.WebSocket.NET.Types.OutputState.Started:
-                        case OBS.WebSocket.NET.Types.OutputState.Starting:
-                            FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
+        //    if (CurrentSource == 0)
+        //    {
+        //        RunOnUiThread(() =>
+        //        {
+        //            //recording started / stopped
+        //            switch (type)
+        //            {
+        //                case OBS.WebSocket.NET.Types.OutputState.Started:
+        //                case OBS.WebSocket.NET.Types.OutputState.Starting:
+        //                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Visible;
 
-                            break;
-                        case OBS.WebSocket.NET.Types.OutputState.Stopped:
-                            FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
-                            break;
-                    }
-                });
-            }
-        }
+        //                    break;
+        //                case OBS.WebSocket.NET.Types.OutputState.Stopped:
+        //                    FindViewById(Resource.Id.rec).Visibility = ViewStates.Gone;
+        //                    break;
+        //            }
+        //        });
+        //    }
+        //}
 
-        private void _obs_StreamingStateChanged(ObsWebSocket sender, OBS.WebSocket.NET.Types.OutputState type)
-        {
-            if (type == OutputState.Started)
-                STREAMING = true;
-            else
-                STREAMING = false;
+        //private void _obs_StreamingStateChanged(ObsWebSocket sender, OBS.WebSocket.NET.Types.OutputState type)
+        //{
+        //    if (type == OutputState.Started)
+        //        STREAMING = true;
+        //    else
+        //        STREAMING = false;
 
-            if (CurrentSource == 0)
-            {
-                RunOnUiThread(() =>
-                {
-                //streaming started / stopped
-                switch (type)
-                    {
-                        case OBS.WebSocket.NET.Types.OutputState.Started:
-                        case OBS.WebSocket.NET.Types.OutputState.Starting:
-                            FindViewById(Resource.Id.live).Visibility = ViewStates.Visible;
+        //    if (CurrentSource == 0)
+        //    {
+        //        RunOnUiThread(() =>
+        //        {
+        //        //streaming started / stopped
+        //        switch (type)
+        //            {
+        //                case OBS.WebSocket.NET.Types.OutputState.Started:
+        //                case OBS.WebSocket.NET.Types.OutputState.Starting:
+        //                    FindViewById(Resource.Id.live).Visibility = ViewStates.Visible;
 
-                            break;
-                        case OBS.WebSocket.NET.Types.OutputState.Stopped:
-                            FindViewById(Resource.Id.live).Visibility = ViewStates.Gone;
-                            break;
-                    }
-                });
-            }
-        }
+        //                    break;
+        //                case OBS.WebSocket.NET.Types.OutputState.Stopped:
+        //                    FindViewById(Resource.Id.live).Visibility = ViewStates.Gone;
+        //                    break;
+        //            }
+        //        });
+        //    }
+        //}
 
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
---
+++ ObsTallyLight/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace ObsTallyLight
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            
+        }
+
+        public override void OnBackPressed() { }
+
+        // Launches the startup task
+        protected async override void OnResume()
+        {
+            base.OnResume();
+            await Task.Delay(1000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+    }
+}
